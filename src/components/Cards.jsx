f2466c3f1e83b8fa307e7e7b094135edb17f45d1
Edo <theeduardo9800@gmail.com>
2025-02-26 18:46:49 +0100

    arreglado error en el import

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { currencyRates } from "../data/currencyRates";
+import { currencyRates } from "../data/currency";
 
 export default function Cards({ courses }) {
   const [selectedTag, setSelectedTag] = useState("All");
